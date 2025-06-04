c4da77edf578ade42f36f787ce37b81d7c676f34
Ritesh <ritesh.sargam00@gmail.com>
2025-06-04 11:57:39 +0530

    Swap 2 numbers without using 3rd variable

---
+++ LogicalProgramsCSharp/SwapNumbers1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class SwapNumbers1
+    {
+        static void Main()
+        {
+            int a = 342, b = 784;
+            Console.WriteLine($"Numbers Before Swap: a => {a}; b => {b}");
+            a = a * b; b = a / b; a = a / b;
+            Console.WriteLine($"Numbers After Swap: a => {a}; b => {b}");
+            Console.ReadLine();
+        }
+    }
+    internal class SwapNumbers2      //Solution 2
+    {
+        static void Main()
+        {
+            Console.Write("Enter 1st number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Numbers Before Swap: a => {a}; b => {b}");
+            a = a + b; b = a - b; a = a - b;
+            Console.WriteLine($"Numbers After Swap: a => {a}; b => {b}");
+            Console.ReadLine();
+        }
+    }
+
+}
