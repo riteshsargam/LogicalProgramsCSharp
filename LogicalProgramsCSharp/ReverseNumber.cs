c6bcef62e0183ace0ccda9bb2d1d0b4cc3d59eea
Ritesh <ritesh.sargam00@gmail.com>
2025-06-05 21:08:50 +0530

    Reverse Number

---
+++ LogicalProgramsCSharp/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class ReverseNumber
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int Number = int.Parse(Console.ReadLine());
+            int Reminder, Reverse = 0;
+            while (Number != 0)
+            {
+                Reminder = Number % 10;
+                Reverse = Reverse * 10 + Reminder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reversed Number is: " + Reverse);
+            Console.ReadLine();
+        }
+    }
+}
