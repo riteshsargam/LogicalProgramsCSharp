52fc49ac9193c0a03fdeaffc6a186673bc513cc2
Ritesh <ritesh.sargam00@gmail.com>
2025-06-12 22:23:31 +0530

    Sum of Digits

---
+++ LogicalProgramsCSharp/SumOfDigits1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class SumOfDigits1
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number to find sum of its digits: ");
+            int Number = int.Parse(Console.ReadLine());
+            int Reminder, Sum = 0;
+            while (Number > 0)
+            {
+                Reminder = Number % 10;
+                Sum = Sum + Reminder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Sum of the digits of given no is: " + Sum);
+            Console.ReadLine();
+        }
+    }
+}
