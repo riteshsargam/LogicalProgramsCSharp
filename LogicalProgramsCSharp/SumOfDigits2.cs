ab266d84586e1734a28b5448175bf410043fc7e3
Ritesh <ritesh.sargam00@gmail.com>
2025-06-13 12:23:26 +0530

    Sum of digits of a given number until single digit

---
+++ LogicalProgramsCSharp/SumOfDigits2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class SumOfDigits2
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number to find sum of it's digits: ");
+            int Number = int.Parse(Console.ReadLine());
+            int Reminder, Sum = 0;
+            do
+            {
+                if (Sum != 0)
+                {
+                    Number = Sum;
+                    Sum = 0;
+                }
+                while (Number > 0)
+                {
+                    Reminder = Number % 10;
+                    Sum = Sum + Reminder;
+                    Number = Number / 10;
+                }
+            }
+            while (Sum > 9);
+            Console.WriteLine("Sum of the digits of given no is: " + Sum);
+            Console.ReadLine();
+        }
+    }
+}
