5e6421f096ca206f0d1ad1211394b924958c8183
Ritesh <ritesh.sargam00@gmail.com>
2025-06-17 22:41:25 +0530

    String Reverse

---
+++ LogicalProgramsCSharp/StringReverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class StringReverse
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string reverse = "";
+            foreach (char ch in input)
+                reverse = ch + reverse;
+            Console.WriteLine($"Reverse of given string '{input}' is: '{reverse}'");
+            Console.ReadLine();
+        }
+    }
+}
