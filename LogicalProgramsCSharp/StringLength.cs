ed3372986dedc0ba925157b9c32d67cea90848de
Ritesh <ritesh.sargam00@gmail.com>
2025-06-19 21:22:43 +0530

    Length of a given string

---
+++ LogicalProgramsCSharp/StringLength.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class StringLength
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            int Length = 0;
+            foreach (char ch in input)
+                Length += 1;
+            Console.WriteLine("Length of given string is: " + Length);
+            Console.ReadLine();
+        }
+    }
+}
