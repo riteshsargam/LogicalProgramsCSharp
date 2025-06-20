0e3520b32604d1b005dad78a39fa6ab1b586089c
Ritesh <ritesh.sargam00@gmail.com>
2025-06-20 09:37:24 +0530

    no. of characters in each string

---
+++ LogicalProgramsCSharp/CharCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class CharCount
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            int Length = 0;
+            foreach (char ch in input)
+            {
+                if (ch != 32)
+                    Length += 1;
+            }
+            Console.WriteLine("No. of char's in given string are: " + Length);
+            Console.ReadLine();
+        }
+    }
+}
