888894f9089297a9a16a23659caf7234b0818e67
Ritesh <ritesh.sargam00@gmail.com>
2025-06-21 13:21:12 +0530

    Reverse Number

---
+++ LogicalProgramsCSharp/ReverseWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class ReverseWords
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string word = "", reverseWords = "";
+            foreach (char ch in input)
+            {
+                if (ch != 32)
+                    word = word + ch;
+                else
+                {
+                    reverseWords = " " + word + reverseWords;
+                    word = "";
+                }
+            }
+            if (word != "")
+                reverseWords = word + reverseWords;
+            Console.WriteLine(reverseWords);
+            Console.ReadLine();
+        }
+    }
+}
