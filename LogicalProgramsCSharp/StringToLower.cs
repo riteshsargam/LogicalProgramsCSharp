3cedc228558b980c0ec34dcaf223296cf836ebd5
Ritesh <ritesh.sargam00@gmail.com>
2025-06-22 21:11:21 +0530

    Given string into lower case

---
+++ LogicalProgramsCSharp/StringToLower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class StringToLower
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string output = "";
+            foreach (char ch in input)
+            {
+                if (ch >= 65 && ch <= 90)
+                    output += (char)(ch + 32);
+                else
+                    output += ch;
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
