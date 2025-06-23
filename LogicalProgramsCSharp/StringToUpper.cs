b1d2fcfeb885967087e499915b7ee598688eb0d0
Ritesh <ritesh.sargam00@gmail.com>
2025-06-23 11:07:37 +0530

    Given string into upper case

---
+++ LogicalProgramsCSharp/StringToUpper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class StringToUpper
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string output = "";
+            foreach (char ch in input)
+            {
+                if (ch >= 97 && ch <= 122)
+                    output += (char)(ch - 32);
+                else
+                    output += ch;
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
