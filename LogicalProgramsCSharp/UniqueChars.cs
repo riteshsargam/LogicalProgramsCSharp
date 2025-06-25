6e8cd625215227fcb46038128e48b3c597cdb284
Ritesh <ritesh.sargam00@gmail.com>
2025-06-25 11:22:24 +0530

    Unique characters in each string

---
+++ LogicalProgramsCSharp/UniqueChars.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class UniqueChars
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            bool Exists = false;
+            int Count1 = 0, Count2 = 0;
+            foreach (char ch1 in input)
+            {
+                Count1 += 1;
+                foreach (char ch2 in input)
+                {
+                    Count2 += 1;
+                    if (Count1 != Count2)
+                    {
+                        if (ch1 != ch2 && ch1 != 32)
+                            Exists = false;
+                        else
+                        {
+                            Exists = true;
+                            break;
+                        }
+                    }
+                }
+                if (Exists == false)
+                    Console.Write(ch1);
+                Count2 = 0; Exists = false;
+            }
+            Console.ReadLine();
+        }
+    }
+}
