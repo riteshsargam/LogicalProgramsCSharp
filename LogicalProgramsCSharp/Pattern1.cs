950c73c22d03980f7ec4ff8e5caa64078419ecd3
Ritesh <ritesh.sargam00@gmail.com>
2025-07-03 19:45:06 +0530

    Numbers Increasing Pattern1

---
+++ LogicalProgramsCSharp/Pattern1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsCSharp
+{
+    internal class Pattern1
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
